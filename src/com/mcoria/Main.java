e2bcb310060ee81b79cff471a988cd1c726fa231
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-22 18:01:49 -0500

    learning about comments

--- src/com/mcoria/Main.java
+++ src/com/mcoria/Main.java
@@ -3,6 +3,7 @@ package com.mcoria;
 public class Main {
 
     public static void main(String[] args) {
+        //write your code here
         char a = 'A';
         System.out.println("Hooray my first java app");
     }
