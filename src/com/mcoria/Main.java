05f35c0eea453b99b43b464832ec510c103570d2
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-22 18:07:13 -0500

    making variable

--- src/com/mcoria/Main.java
+++ src/com/mcoria/Main.java
@@ -3,6 +3,9 @@ package com.mcoria;
 public class Main {
 
     public static void main(String[] args) {
+
+        // state the type then the name and value
         int number = 100;
+        String brand = "Algomaster";
     }
 }
