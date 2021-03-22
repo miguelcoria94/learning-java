266e288ca4d18b4dba5a0e08a051c29d7cb8a551
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-22 18:51:03 -0500

    working with Math

--- src/com/mcoria/Main.java
+++ src/com/mcoria/Main.java
@@ -1,12 +1,12 @@
 package com.mcoria;
 
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-        int zero = 0;
-        int one = 1;
-        double pi = 2.1415;
-        int subscriberCount = 1_000_000;
+
     }
 }
