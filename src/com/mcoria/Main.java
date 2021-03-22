5da997e1a8cde705deb90a9e7e411f3bb81120c5
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-22 18:06:26 -0500

    making a number

--- src/com/mcoria/Main.java
+++ src/com/mcoria/Main.java
@@ -3,8 +3,6 @@ package com.mcoria;
 public class Main {
 
     public static void main(String[] args) {
-        //write your code here
-        char a = 'A';
-        System.out.println("Hooray my first java app");
+        int number = 100;
     }
 }
