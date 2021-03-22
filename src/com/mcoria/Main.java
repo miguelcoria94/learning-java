74814f68c7b3b5b958c4b373ff3c477b7fe97cde
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-22 18:30:12 -0500

    creating variable names

--- src/com/mcoria/Main.java
+++ src/com/mcoria/Main.java
@@ -4,24 +4,9 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-
-        // state the type then the name and value
-        int number = 100;
-        String brand = "Algomaster";
-        double num = 100.01;
-        Date date = new Date();
-
-        byte theByte = 120;
-        short theShort = 1200;
-        int theInt = 13000;
-        long theLong = 230950825804L;
-        float pi = 3.14F;
-        double doublePi = 3.1334;
-
-        boolean isAdult = true;
-
-        char nameInitial = 'A';
-
-        System.out.println(date);
+        int zero = 0;
+        int one = 1;
+        double pi = 2.1415;
+        int subscriberCount = 1_000_000;
     }
 }
