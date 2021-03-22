67b85710a3be941cef1a3293c9d98bd689f24280
miguelcoria94 <miguelcoria94@gmail.com>
2021-03-22 18:14:24 -0500

    using more data types

--- src/com/mcoria/Main.java
+++ src/com/mcoria/Main.java
@@ -1,5 +1,6 @@
 package com.mcoria;
 
+import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
@@ -7,5 +8,20 @@ public class Main {
         // state the type then the name and value
         int number = 100;
         String brand = "Algomaster";
+        double num = 100.01;
+        Date date = new Date();
+
+        byte theByte = 120;
+        short theShort = 1200;
+        int theInt = 13000;
+        long theLong = 230950825804L;
+        float pi = 3.14F;
+        double doublePi = 3.1334;
+
+        boolean isAdult = true;
+
+        char nameInitial = 'A';
+
+        System.out.println(date);
     }
 }
